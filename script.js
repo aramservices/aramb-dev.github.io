1ec2cade9cc128555a8554c44e665edde314ef2a
Aram Services <aramservices198@hotmail.com>
2021-02-28 20:08:56 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+//main.js
+
+function darkmode() {
+  const wasDarkmode = localStorage.getItem('darkmode') === 'true';
+  localStorage.setItem('darkmode', !wasDarkmode);
+  const element = document.body;
+  element.classList.toggle('dark-mode', !wasDarkmode);
+}
+
+function onload() {
+  document.body.classList.toggle('dark-mode', localStorage.getItem('darkmode') === 'true');
+}
