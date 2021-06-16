b546c15e9f1183f34b8741dc94c5476c34231c4d
Aram Services <aramservices198@gmail.com>
2021-06-16 07:51:37 -0400

    Deleting
    Signed-off-by: Aram Services <aramservices198@gmail.com>

--- assets/solidity/Var-Types.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity 0.5.1;
-contract Var_TypesCon {
-    string public stringVar = "Default";
-    bool public boolvar = true;
-    int public intvar = 1;
-    uint public uintvar = 1;
-    uint8 public uint8var = 8;
-    uint256 public uint256var = 99;
-}
